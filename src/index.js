4f9da136e5118a97e1bd891513dc87cc3fa4fe6e
Darshan Datta <darshandatta.10@gmail.com>
2019-06-02 01:10:56 +0530

    enabled authentication on landing page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-import './scss/styles.scss'
+import "./scss/index.scss";
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+import firebaseConfig from "./config";
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// FirebaseUI config
+import * as firebaseui from "firebaseui";
+const uiConfig = {
+  signInSuccessUrl: "/notes.html",
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+    //   firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    //   firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  // tosUrl and privacyPolicyUrl accept either url string or a callback
+  // function.
+  // Terms of service url/callback.
+  tosUrl: "<your-tos-url>",
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function() {
+    window.location.assign("<your-privacy-policy-url>");
+  }
+};
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start("#firebaseui-auth-container", uiConfig);
